189a5d8635a69a65adf296205c7017c36cdb6c41
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-02-16 23:20:11 +0100

    Add onClick

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,34 @@
 import './Card.css'
+import React from 'react'
 
-export default function Card({ name, species, image }) {
+export default function Card({
+    name,
+    species,
+    image,
+    status,
+    gender,
+    origin,
+    location,
+}) {
+    const [isDetailsVisible, setIsDetailsVisible] = React.useState(false)
     return (
         <section className="Card">
             {name} {species === 'Human' ? '👤' : '👽'}
             <img src={image} alt="" />
+            <button
+                onClick={(event) => {
+                    event.stopPropagation()
+                    setIsDetailsVisible(!isDetailsVisible)
+                }}
+            >
+                {isDetailsVisible ? 'Hide details' : 'Show details'}
+            </button>
+            {isDetailsVisible && (
+                <p>
+                    {status} <br></br> {gender} <br></br> {origin} <br></br>
+                    {location}
+                </p>
+            )}
         </section>
     )
 }
