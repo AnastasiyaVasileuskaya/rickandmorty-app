0eaffd24cc2d73846aaf88620276de4255b9729a
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-02-16 15:12:29 +0100

    Add App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { results } from './rickandmortyapi.json'
+import Card from './Card'
+import './App.css'
+
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    return (
+        <div className="App">
+            {results.map(({ name, id, species, image }) => (
+                <Card key={id} name={name} species={species} image={image} />
+            ))}
+        </div>
+    )
 }
 
-export default App;
+export default App
